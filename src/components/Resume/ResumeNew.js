a1486493da3f48f4a4bb97fea1c9851f8c3bb87a
sandipmahato248 <sandipmahato248@gamil.com>
2024-07-14 13:51:00 -0500

    test

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -10,7 +10,7 @@ import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
+//check
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
